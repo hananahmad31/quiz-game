332ad5eb83bbce36e0b1773141406a22fabd1cc1
Muhammad Umer <muhammad.umer@voltstudios.mx>
2024-05-09 22:08:48 +0500

    Screens

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(QuizApp());
+}
+
+class QuizApp extends StatelessWidget {
+  const QuizApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: Placeholder(),);
+  }
+}
